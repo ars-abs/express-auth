8477020a84698bd24aff23e3d2431c694f196558
ars-abs <ars@abstractit.in>
2023-05-25 12:44:44 +0530

    Introduced endpoint /.

--- example/index.js
+++ example/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from '../config';
 import cors from 'cors';
 import expressAuth from '../src';
+import cookieParser from 'cookie-parser';
 
 process.env.PORT = 1234;
 const { PORT } = process.env;
@@ -10,6 +11,11 @@ process.env.URL = `http://localhost:${ PORT }`;
 const app = express();
 
 app.use(cors({ origin: '*' }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+	res.send('homepage');
+});
 
 expressAuth({ app, config });
 
