339b371327c22dc953b8b28827eb12513bd964ea
ars-abs <ars@abstractit.in>
2023-05-25 12:48:24 +0530

    ignore some files

--- example/index.js
+++ example/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import config from '../config';
+import config from './.config';
 import cors from 'cors';
 import expressAuth from '../src';
 import cookieParser from 'cookie-parser';
