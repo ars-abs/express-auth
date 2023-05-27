1d7a3a1919c854370052fb5afb08452103332698
ars-abs <ars@abstractit.in>
2023-05-27 13:36:50 +0530

    refactored.

--- src/saveLogin.js
+++ src/saveLogin.js
@@ -1,18 +1,26 @@
+import { findIndex } from '@laufire/utils/collection';
 import saveTokens from './saveTokens';
 import jwt from 'jsonwebtoken';
 
-const saveLogin = async ({ user: { idToken, ...restTokens }}, res) => {
+const saveLogin = async ({ user: { idToken, ...restTokens },
+	context: { config: { auth: { providers }}}}, res, next) => {
 	const { sub, iss } = jwt.decode(idToken);
+
+	const issuer = findIndex(providers,
+		({ issuer: provider }) => provider === iss);
 	const token = jwt.sign(
-		{ sub: sub, iss: iss, role: 'user' }, 'secret', { expiresIn: '1h' }
+		{
+			sub: sub,
+			iss: issuer,
+			role: 'user',
+		}, 'secret', { expiresIn: '1h' }
 	);
 
-	await saveTokens({ ...restTokens, sub, iss });
-
+	await saveTokens({ ...restTokens, sub, issuer });
 	res.cookie(
 		'token', token, { httpOnly: true, secure: true }
 	);
-	res.redirect('/');
+	next();
 };
 
 export default saveLogin;
--- src/saveTokens.js
+++ src/saveTokens.js
@@ -1,11 +1,11 @@
 import User from './User';
 
-const saveTokens = async ({ sub, iss, refreshToken, accessToken }) => {
+const saveTokens = async ({ sub, issuer, refreshToken, accessToken }) => {
 	await User.findOrCreate({
 		where: { user: sub },
 		defaults: {
 			user: sub,
-			iss: iss,
+			iss: issuer,
 			refreshToken: refreshToken,
 			accessToken: accessToken,
 		},
