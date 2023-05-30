687bc3115611b62cd31dee0eb2fc9c0a672a91e2
ars-abs <ars@abstractit.in>
2023-05-30 10:38:09 +0530

    Bug fixed always redirect to /renewToken.

--- src/renewTokens/index.js
+++ src/renewTokens/index.js
@@ -1,14 +1,14 @@
 import refreshTokens from './refreshTokens';
 
 const renewTokens = async (req, res, next) => {
-	const refreshing = async (request) => {
-		request.user = await refreshTokens(request);
-		next();
+	const refreshing = async (request, response) => {
+		request.user = await refreshTokens(request, response);
+		request.user && next();
 	};
 
 	!req.cookies.token
 		? res.redirect('/login')
-		: await refreshing(req);
+		: await refreshing(req, res);
 };
 
 export default renewTokens;
--- src/renewTokens/refreshTokens.js
+++ src/renewTokens/refreshTokens.js
@@ -1,15 +1,20 @@
 import jwt from 'jsonwebtoken';
 import fetchTokens from './fetchTokens';
 
-const refreshTokens = async (req) => {
+const refreshTokens = async (req, res) => {
 	const { sub, iss } = jwt.decode(req.cookies.token);
 	const {
 		tokenURL, clientID, clientSecret,
 	} = req.context.config.auth.providers[iss];
-	const [{ refreshToken }] = await req.context.config.auth.userSchema
-		.findAll({ where: { user: sub, iss: iss }});
+	const user = (await req.context.config.auth.userSchema
+		.findAll({ where: { user: sub, iss: iss }}))[0];
 
-	return fetchTokens({ refreshToken, tokenURL, clientID, clientSecret });
+	return user
+		// eslint-disable-next-line object-shorthand
+		? fetchTokens({
+			refreshToken: user.refreshToken, tokenURL, clientID, clientSecret,
+		})
+		: res.redirect('/login');
 };
 
 export default refreshTokens;
