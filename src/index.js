a73662f63303ab5264776125896bddf1e687aa97
dmdm-abs <dmdm@abstractit.in>
2023-06-19 16:30:57 +0530

    Refactored.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import setupAuthFlows from './setup/setupAuthFlows';
 import setupLogout from './setup/setupLogout';
 import setupRenewTokens from './setup/setupRenewTokens';
 import buildVerifier from './setup/buildVerifier';
+import { runSteps } from './help[ers';
 
 const expressAuth = (context) => {
-	const enrichedContext = enrichContext(context);
-
-	setupRequestParsers(enrichedContext);
-	setupAuthFlows(enrichedContext);
-	setupLogout(enrichedContext);
-	setupRenewTokens(enrichedContext);
+	runSteps([
+		setupRequestParsers,
+		setupAuthFlows,
+		setupLogout,
+		setupRenewTokens,
+	], enrichContext(context));
 
 	return buildVerifier();
 };
