6d742684e8f1203c3e05c1f5be6e922aa5ef570e
ars-abs <ars@abstractit.in>
2023-06-19 14:01:59 +0530

    Restructured buildVerifier.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import passport from 'passport';
-import setupVerifier from './setup/setupVerifier';
 import enrichContext from './enrichContext';
+import setupRequestParsers from './setup/setupRequestParsers';
 import setupAuthFlows from './setup/setupAuthFlows';
 import setupLogout from './setup/setupLogout';
 import setupRenewTokens from './setup/setupRenewTokens';
-import setupRequestParsers from './setup/setupRequestParsers';
+import buildVerifier from './setup/buildVerifier';
 
 const expressAuth = (context) => {
 	const enrichedContext = enrichContext(context);
-	const { app } = enrichedContext;
 
 	setupRequestParsers(enrichedContext);
 	setupAuthFlows(enrichedContext);
-	setupVerifier();
 	setupLogout(enrichedContext);
 	setupRenewTokens(enrichedContext);
-	app.use(passport.authenticate('jwt',
-		{ failureRedirect: '/renewTokens', session: false }));
+
+	return buildVerifier();
 };
 
 export { expressAuth };
--- src/setup/buildVerifier.js
+++ src/setup/buildVerifier.js
@@ -3,11 +3,14 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 const cookieExtractor = (req) => (req && req.cookies ? req.cookies.token : '');
 
-const setupVerifier = () => {
+const buildVerifier = () => {
 	passport.use('jwt', new JwtStrategy({
 		jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
 		secretOrKey: process.env.JWTSECRET,
 	}, (jwtPayload, done) => done(null, jwtPayload)));
+
+	return passport.authenticate('jwt',
+		{ failureRedirect: '/renewTokens', session: false });
 };
 
-export default setupVerifier;
+export default buildVerifier;
