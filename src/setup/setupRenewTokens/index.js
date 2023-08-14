4bfd52185f9b561c507471e30b5e871e6511b857
ars-abs <ars@abstractit.in>
2023-08-14 18:27:02 +0530

    Bug fixed.

--- src/setup/setupRenewTokens/index.js
+++ src/setup/setupRenewTokens/index.js
@@ -1,5 +1,5 @@
+import saveLogin from '../../saveLogin';
 import renewTokens from './renewTokens';
-import saveLogin from '@saveLogin';
 
 const setupRenewTokens = ({ app }) => app.get(
 	'/renewTokens', renewTokens, saveLogin, (req, res) => res.redirect('/')
